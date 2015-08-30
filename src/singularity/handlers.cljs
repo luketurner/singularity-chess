5363d29cc80bcbafbc272d687e1e566363367486
Luke Turner <github@luketurner.org>
2015-08-30 16:36:16 -0700

    Fix minor doc/debug issues

--- src/singularity/handlers.cljs
+++ src/singularity/handlers.cljs
@@ -18,20 +18,23 @@
 
 (defn- move! [s1 s2]
   "Move function accepts 2 coords, moves from s1 to s2"
-  (print "move" s1 s2)
   (db/assoc-in [:board s2 :piece] (db/get-in [:board s1 :piece]))
   (db/assoc-in [:board s1 :piece] nil))
 
 (defn space
+  "Event handler for when user clicks a space. This can have one of two
+  outcomes. If the click indicates a valid move for the selected piece,
+  clicking will move the selected piece to the new location. If the
+  movement is invalid or a piece is not selected, this will select the
+  clicked piece."
   [x y]
-  "Event handler for when user clicks a space"
   (let [coords [x y]
         piece (db/get-in [:board coords :piece])
         selected (db/get :selected-space)]
     (if (or (nil? selected)
             (= selected coords)
             (not (valid-move? selected coords)))
-      (toggle-select! coords)
-      (do
+      (toggle-select! coords) ; not a valid move, use selection
+      (do ; this is a valid move, use movement
         (move! selected coords)
         (toggle-select! nil)))))
